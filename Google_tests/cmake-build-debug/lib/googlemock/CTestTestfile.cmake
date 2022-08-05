dba7fe2e246a32453268e1296f49a38c01210a14
RodionIK <bcons@yandex.ru>
2022-08-05 20:03:28 +0300

    Remove duplicated directory

--- Google_tests/cmake-build-debug/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/rodionkargapolov/CLionProjects/Yandex-praktikum/TransportCatalogue/Google_tests/lib/googlemock
-# Build directory: /Users/rodionkargapolov/CLionProjects/Yandex-praktikum/TransportCatalogue/Google_tests/cmake-build-debug/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
